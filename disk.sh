ab2cf2ab2a8ca788bd629665faf3ae3f0473545d
mickocancino <49170304+mickocancino@users.noreply.github.com>
2019-04-01 13:48:47 -0700

    Create disk.sh

---
+++ disk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /home && find . -maxdepth 2 -type f -name 'backup-*.tar.gz' | grep -v 'virtfs' > /tmp/tar
+more /tmp/tar | awk '{ print "mv -f /home/"$1" /backup2/vps10/" }' > /tmp/move
+more /tmp/move
+
+
+
+echo " "
+echo " "
+
+cd /home && find . -maxdepth 2 -type d -name 'backup-*' | grep -v 'virtfs' > /tmp/dir
+more /tmp/dir | awk  '{ print "rm -Rf /home/"$1 }' > /tmp/remdir
+more /tmp/remdir
